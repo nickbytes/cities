0d1e3769bc81e0a9b82868dc225795fea7a60f87
Nick Beattie <nick@nickbytes.com>
2024-04-08 11:09:03 -0700

    init

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import * as fs from "fs";
+import csv from "csv-parser";
+
+const filePath = "./california.csv";
+
+fs.createReadStream(filePath)
+  .pipe(csv())
+  .on("data", (data: any) => {
+    console.log(data);
+  })
+  .on("end", () => {
+    console.log("End of file");
+  });
